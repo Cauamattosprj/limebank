2ba1227c81fbff30070bec79390ff914905f6d28
Caua Mattos <caua.h4k@gmail.com>
2025-06-15 20:17:18 -0300

    Initial commit

---
+++ src/main/java/com/cauamattosprj/limebank/LimebankApplication.java
@@ -0,0 +1,13 @@
+package com.cauamattosprj.limebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimebankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimebankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cauamattosprj/limebank/LimebankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cauamattosprj.limebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimebankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
