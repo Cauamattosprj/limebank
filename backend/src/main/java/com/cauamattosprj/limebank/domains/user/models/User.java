a3332ac38231c280c469f076391b69f707793926
Caua Mattos <caua.h4k@gmail.com>
2025-06-19 22:08:37 -0300

    feat(security): Implement UserDetails interface in User model

--- backend/src/main/java/com/cauamattosprj/limebank/domains/user/models/User.java
+++ backend/src/main/java/com/cauamattosprj/limebank/domains/user/models/User.java
@@ -4,14 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NonNull;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.Instant;
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @AllArgsConstructor
 @Data
 @Builder
-public class User {
+public class User implements UserDetails {
     private UUID id;
     private UUID customer_id;
 
@@ -26,4 +30,34 @@ public class User {
     private Instant updated_at;
     private Instant deleted_at;
     private boolean is_deleted;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(() -> "ROLE_" + role.toUpperCase());
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
