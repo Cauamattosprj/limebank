4cbaa3deec2e8728e659c1e09e47fb7fbe8ba636
Caua Mattos <caua.h4k@gmail.com>
2025-06-22 10:25:32 -0300

    feat(api response): add an custom  exception for duplicate emails trying to be registered

---
+++ backend/src/main/java/com/cauamattosprj/limebank/domains/auth/exceptions/EmailAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.cauamattosprj.limebank.domains.auth.exceptions;
+
+public class EmailAlreadyExistsException extends RuntimeException {
+    public EmailAlreadyExistsException(String message) {
+        super(message);
+    }
+}
